ad7b044ee27308d993189af4fd9168b4435283ad
Joshua J <jj4440024@gmail.com>
2026-07-15 10:52:54 +0530

    Create view module routing and navigation

--- src/components/ViewMembers.jsx
+++ src/components/ViewMembers.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 const ViewMembers = () => {
-  return (
-    <div>ViewMembers</div>
-  )
+    return (
+        <div className="container mt-5">
+            <div className="card">
+                <div className="card-header bg-primary text-white">
+                    <h3 className="text-center">View Members</h3>
+                </div>
+
+                <div className="card-body text-center">
+                    Members Table
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ViewMembers
