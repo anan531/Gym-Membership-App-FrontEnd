69983c59da3edb38de1acb2c7026f678f6f528f5
vincentcibin-a11y <vincentcibin@gmail.com.com>
2026-07-15 12:34:23 +0530

    Added Home, Add Member, Add Trainer, Add Workout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import NavigationBar from './components/NavigationBar'
+import AddMember from './components/AddMember'
+import ViewMember from './components/ViewMember'
+import AddWorkout from './components/AddWorkout'
+import ViewWorkout from './components/ViewWorkout'
+import AddTrainer from './components/AddTrainer'
+import ViewTrainer from './components/ViewTrainer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+
+      <NavigationBar />
+
+      <Routes>
+        <Route path="/" element={<AddMember />} />
+        <Route path="/addmember" element={<AddMember />} />
+        <Route path="/viewmember" element={<ViewMember />} />
+        <Route path="/addworkout" element={<AddWorkout />} />
+        <Route path="/viewworkout" element={<ViewWorkout />} />
+        <Route path="/addtrainer" element={<AddTrainer />} />
+        <Route path="/viewtrainer" element={<ViewTrainer />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
+
 
 export default App
